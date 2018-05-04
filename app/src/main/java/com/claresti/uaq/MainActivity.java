fdf1282e765a18a62f11cb2514e864e92ed55ad5
Elias Rosales <mansion1@hotmail.com>
2018-05-04 00:49:35 -0500

    Se concreto el la pantalla de inicio

--- app/src/main/java/com/claresti/uaq/MainActivity.java
+++ app/src/main/java/com/claresti/uaq/MainActivity.java
@@ -6,48 +6,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import com.github.clans.fab.FloatingActionMenu;
-
 public class MainActivity extends AppCompatActivity{
 
-    FloatingActionMenu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        menu = findViewById(R.id.menu);
-        menu.setClosedOnTouchOutside(true);
     }
 
-    public void Tvuaq(View view){
+    public void btnTv(View view){
         Intent i = new Intent(MainActivity.this, tvUAQ.class);
         startActivity(i);
     }
 
-    public void Ruaq(View view){
+    public void btnRadio(View view){
         Intent i = new Intent(MainActivity.this, radioUAQ.class);
         startActivity(i);    }
 
-    public void Alumnos(View view){
+    public void btnAlumnos(View view){
         Intent i = new Intent(MainActivity.this, alumnos.class);
         startActivity(i);
     }
 
-    public void Docentes(View view){
+    public void btnDocentes(View view){
         Intent i = new Intent(MainActivity.this, docentes.class);
         startActivity(i);
     }
-    public void Acerca(View view){
+    public void btnAcerca(View view){
         Intent i = new Intent(MainActivity.this, Acerca.class);
         startActivity(i);
     }
 
-    public void Facultades(View view){
+    public void btnFacultades(View view){
         Toast.makeText(this,"Esta ventana se encontrara proximamnete!! ;)", Toast.LENGTH_SHORT).show();
     }
-    public void Contacto(View view){
+    public void btnContacto(View view){
         Toast.makeText(this,"Esta ventana se encontrara proximamnete!! ;)", Toast.LENGTH_SHORT).show();
     }
 
